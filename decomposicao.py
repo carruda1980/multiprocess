d5af1df410922cd660e81607618d69cf9b7f2f90
Carlos Arruda <caugustogarruda@gmail.com>
2020-09-14 21:12:42 -0300

    Class worker

--- decomposicao.py
+++ decomposicao.py
@@ -1,5 +1,6 @@
+from time import sleep
 from urllib.parse import urljoin
-from threading import Event
+from threading import Event, Thread
 from queue import Queue
 from requests import get
 
@@ -13,6 +14,36 @@ def get_urls():
     [fila.put(pokemon) for pokemon in pokemons]
     event.set()
     fila.put('Kill')
-    import ipdb; ipdb.set_trace()
+    #import ipdb; ipdb.set_trace()
 
+
+class Worker(Thread):
+    def __init__(self, target, queue, *, name='Worker'):
+        super.__init__()
+        self.name = name
+        self.queue = queue
+        self._target = target
+        self._stoped = False
+
+        print(self.name, 'Started')
+
+    def run(self):
+        #event.wait()
+
+        while not self.queue.empty():
+            pokemon = self.queue.get()
+            print(self.name, pokemon)
+
+            if pokemon == "Kill":
+                self.queue.put(pokemon)
+                self._stoped = True
+                break
+
+            self._target(pokemon)
+
+    def join(self):
+        while not self._stoped:
+            sleep(0.1)
+
+            
 get_urls()
